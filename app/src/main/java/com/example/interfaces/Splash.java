0a32ba28c081e06ff8419a5aa2e79ba6b4be9b7a
curso <miguelmoneocarmona@gmail.com>
2020-11-13 13:58:18 +0100

    splah

---
+++ app/src/main/java/com/example/interfaces/Splash.java
@@ -0,0 +1,46 @@
+package com.example.interfaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView myImage = (ImageView)findViewById(R.id.imageView4);
+
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.shake);
+
+        View mySubtitle = null;
+        mySubtitle.startAnimation(myanim);
+        openApp(true);
+    }
+
+
+    private void openApp(boolean locationPermission) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash
+                        .this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+
+}
